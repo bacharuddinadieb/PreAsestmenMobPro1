ca3b586eccad405795dc0f58bcdb6c1a5a48b205
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-02-25 10:01:12 +0700

    fragment navigate dan navigate up persegi panjang & segitiga

--- app/src/main/java/d3rpla_4202_6706180113/preasestment1mobpro/HomeFragment.kt
+++ app/src/main/java/d3rpla_4202_6706180113/preasestment1mobpro/HomeFragment.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import d3rpla_4202_6706180113.preasestment1mobpro.databinding.FragmentHomeBinding
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,13 +21,18 @@ private const val ARG_PARAM2 = "param2"
  *
  */
 class HomeFragment : Fragment() {
-
+    lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        binding.btPersegiPanjang.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_persegiPanjangFragment))
+        binding.btSegitiga.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_segitigaFragment))
+
+        return binding.root
     }
 
 
--- app/src/main/java/d3rpla_4202_6706180113/preasestment1mobpro/MainActivity.kt
+++ app/src/main/java/d3rpla_4202_6706180113/preasestment1mobpro/MainActivity.kt
@@ -2,11 +2,19 @@ package d3rpla_4202_6706180113.preasestment1mobpro
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        NavigationUI.setupActionBarWithNavController(this, this.findNavController(R.id.fragmentUtama))
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return this.findNavController(R.id.fragmentUtama).navigateUp()
     }
 }
