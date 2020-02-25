f96a3e70522a4bedd112d64a0ed5302c649065b7
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-02-25 10:31:03 +0700

    penambahan fungsi hitung persegipanjang dan segitiga

--- app/src/main/java/d3rpla_4202_6706180113/preasestment1mobpro/PersegiPanjangFragment.kt
+++ app/src/main/java/d3rpla_4202_6706180113/preasestment1mobpro/PersegiPanjangFragment.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import d3rpla_4202_6706180113.preasestment1mobpro.databinding.FragmentPersegiPanjangBinding
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,13 +20,32 @@ private const val ARG_PARAM2 = "param2"
  *
  */
 class PersegiPanjangFragment : Fragment() {
-
+    lateinit var binding: FragmentPersegiPanjangBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_persegi_panjang, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_persegi_panjang, container, false)
+
+        binding.btHitungPersegiPanjang.setOnClickListener {
+            val hasilKeliling =
+                hitungKeliling(binding.etPanjang.text.toString().toInt(), binding.etLebar.text.toString().toInt())
+            val hasilLuas =
+                hitungLuas(binding.etPanjang.text.toString().toInt(), binding.etLebar.text.toString().toInt())
+//            binding.tvHasilKelilingPersegiPanjang.setText(hasilKeliling.toString())
+            binding.tvHasilLuasPersegiPanjang.text = hasilLuas.toString()
+        }
+
+        return binding.root
+    }
+
+    fun hitungKeliling(panjang: Int, lebar: Int): Double {
+        return (panjang * 2) + (lebar * 2).toDouble()
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Double {
+        return (panjang * lebar).toDouble()
     }
 
 
--- app/src/main/java/d3rpla_4202_6706180113/preasestment1mobpro/SegitigaFragment.kt
+++ app/src/main/java/d3rpla_4202_6706180113/preasestment1mobpro/SegitigaFragment.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import d3rpla_4202_6706180113.preasestment1mobpro.databinding.FragmentSegitigaBinding
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,14 +20,32 @@ private const val ARG_PARAM2 = "param2"
  *
  */
 class SegitigaFragment : Fragment() {
-
+    lateinit var binding: FragmentSegitigaBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_segitiga, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_segitiga, container, false)
+
+        binding.btHitungSegitiga.setOnClickListener {
+            val hasilKeliling =
+                hitungKeliling(binding.etAlas.text.toString().toInt(), binding.etTinggi.text.toString().toInt())
+            val hasilLuas = hitungLuas(binding.etAlas.text.toString().toInt(), binding.etTinggi.text.toString().toInt())
+            binding.tvHasilLuasSegitiga.text = hasilLuas.toString()
+            binding.tvHasilKelilingSegitiga.text = hasilKeliling.toString()
+        }
+
+        return binding.root
     }
 
+    fun hitungKeliling(alas: Int, tinggi: Int): Double {
+        var sisiMiring = Math.sqrt((alas * alas) + (tinggi * tinggi).toDouble())
+        return (alas + tinggi + sisiMiring)
+    }
+
+    fun hitungLuas(alas: Int, tinggi: Int): Double {
+        return (alas * tinggi / 2).toDouble()
+    }
 
 }
