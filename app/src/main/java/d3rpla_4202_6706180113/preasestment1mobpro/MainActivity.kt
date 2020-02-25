2e234d1de750948ee69b16c8bbb5dbd9fd2a1313
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-02-25 09:09:21 +0700

    initial commit

---
+++ app/src/main/java/d3rpla_4202_6706180113/preasestment1mobpro/MainActivity.kt
@@ -0,0 +1,12 @@
+package d3rpla_4202_6706180113.preasestment1mobpro
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
